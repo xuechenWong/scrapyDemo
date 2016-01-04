1e4dcc026cb9920bf01fb03cf9e9409ac45476df
wangxch <wangxch@jiazhengol.com>
2016-01-04 21:34:33 +0800

    update css3 demo.

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('./book',books);
+app.use('/book',books);
 // app.use('/page',pages);
 
 // catch 404 and forward to error handler
